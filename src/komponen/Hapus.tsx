55f0728c0f64fb1c8e5cbd08e3ac7fc2b56386c2
eka312 <fue312@gmail.com>
2025-07-08 10:18:48 +0700

    Merapikan dan menambah Modal pada hapus

--- src/komponen/Hapus.tsx
+++ src/komponen/Hapus.tsx
@@ -8,11 +8,12 @@ type Props = {
 function Hapus({onConfirm, onCancel}: Props) {
   return (
     <div className="modal-overlay">
-        <div className="modal-konten">
+        <div className="modal-konten-hapus">
             <h4>Yakin ingin menghapus projek ini?</h4>
             <div className="modal-actions">
-                <button className='btn-hapus-modal' onClick={onConfirm} > Ya, Hapus</button>
-                <button className='btn-batal-modal' onClick={onCancel} >Batal</button>
+              <button className='btn-batal-modal' onClick={onCancel} >Batal</button>
+              <button className='btn-hapus-modal' onClick={onConfirm} > Ya, Hapus</button>
+                
             </div>
         </div>
     </div>
